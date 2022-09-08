a62698a4730433217334c53bd2cd3ff204b8e6b1
zoobot <rommims@gmail.com>
2022-09-08 05:48:55 -0700

    renamed wtt_area to tree-source Change-type: major
    Signed-off-by: zoobot <rommims@gmail.com>

--- clone_repos.sh
+++ clone_repos.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 CLONE_TYPE='ssh' # or https
+GIT_PREFIX='git@github.com:' # or https://github.com/
 
 if [ "$CLONE_TYPE" == "ssh" ]; then
-    echo "Cloning repos using ssh"
-    # Clones db
-    [[ -d wtt_db ]] || git clone git@github.com:waterthetrees/wtt_db.git
-    # Clones frontend
-    [[ -d wtt_front ]] || git clone git@github.com:waterthetrees/wtt_front.git
-    # Clones server
-    [[ -d wtt_server ]] || git clone git@github.com:waterthetrees/wtt_server.git
+  GIT_PREFIX='git@github.com:'
 fi
 
 if [ "$CLONE_TYPE" == "https" ]; then
-    echo "Cloning repos using https"
-    # Clones db
-    [[ -d wtt_db ]] || git clone https://github.com/waterthetrees/wtt_db.git
-
-    # Clones frontend
-    [[ -d wtt_front ]] || git clone https://github.com/waterthetrees/wtt_front.git
-
-    # Clones server
-    [[ -d wtt_server ]] || git clone https://github.com/waterthetrees/wtt_server.git
+  GIT_PREFIX='https://github.com/'
 fi
+
+echo "Cloning repos"
+# Clones db
+[[ -d wtt_db ]] || git clone "${GIT_PREFIX}waterthetrees/wtt_db.git"
+# Clones frontend
+[[ -d wtt_front ]] || git clone "${GIT_PREFIX}waterthetrees/wtt_front.git"
+# Clones server
+[[ -d wtt_server ]] || git clone "${GIT_PREFIX}waterthetrees/wtt_server.git"
+# Clones tree_sources data
+[[ -d tree-source ]] || git clone "${GIT_PREFIX}waterthetrees/tree-source.git"
+
+
 
 
