acd6f5617ddd95ee442203028dab4cc0297debcb
Jason Zhen <6326660+jazhen@users.noreply.github.com>
2022-09-08 20:23:38 -0700

    Add explanation comments to install script

--- install.sh
+++ install.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-
+# Install nvm. Clones the nvm repository to ~/.nvm, and attempts to export the
+# source lines the correct profile file (~/.bash_profile, ~/.zshrc, ~/.profile, or ~/.bashrc).
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+# Install node 18
 nvm install 18
 nvm use 18
 
-cd wtt_front; npm install; cd ..
-cd wtt_server; npm install; cp .env.example .env; cat .env; cd ..
+# Frontend set up
+cd wtt_front
+npm install
+cd ..
+
+# Backend set up
+cd wtt_server
+npm install
+cp .env.example .env
+cat .env
+cd ..
+
 echo "Done installing npm packages"
-echo "Ask on slack for the .env file and place it in the wtt_server directory"
-
-
+echo "Ask on slack for the environment variables and paste the contents into the .env file"
 
 echo "To get into postgres docker easier"
 echo "Enter password for sudo to echo 127.0.0.1 wtt_postgis to /etc/hosts"
@@ -34,6 +44,7 @@ if [ "${OS_TYPE}" == "Linux" ]; then
   sudo apt-get install git-lfs
 fi
 
+# Db set up
 cd wtt_db
 git lfs install
 git lfs track "treedb.sql"
