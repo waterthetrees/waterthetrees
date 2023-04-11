322a41cd64027376d3ddb843fa72869226a64320
zoobot <rommims@gmail.com>
2023-04-10 17:45:23 -0700

    Fixed clone repos script

--- clone_repos.sh
+++ clone_repos.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ORG="waterthetrees/"
+REPOS=("wtt_db" "wtt_front" "wtt_server" "tree-sources")
 
 has_ssh_permission() {
   echo "Testing ssh permission..."
@@ -9,15 +11,15 @@ clone_repo() {
   if [[ -d $1 ]]; then
     echo "Directory ${1} already exists! Skipping..."
   else
-    git clone "${GIT_PREFIX}waterthetrees/${1}.git"
+    git clone "${GIT_PREFIX}${ORG}${1}.git"
+    # git@github.com:waterthetrees/waterthetrees.git
   fi
 }
 
 clone_all_repos() {
-  clone_repo "wtt_db"
-  clone_repo "wtt_front"
-  clone_repo "wtt_server"
-  clone_repo "tree-sources"
+  for i in $REPOS; 
+    do clone_repo $i; 
+  done
 }
 
 if has_ssh_permission; then
@@ -27,7 +29,7 @@ if has_ssh_permission; then
 else
   echo "ssh failed!"
   echo "Trying to clone using https instead..."
-  GIT_PREFIX='ssh://github.com/'
+  GIT_PREFIX='https://github.com/'
 fi
 
 clone_all_repos || echo "Failed to clone all repos!"
